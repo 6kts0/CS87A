d38b2d50fbcbf4f9ab36cfa78ba9465b0488d15f
6kts0 <fabakouyate06@gmail.com>
2025-11-06 16:20:55 -0800

    FizzBuzz Complete

---
+++ elifStatement.py
@@ -0,0 +1,16 @@
+"""
+FizzBuzz!
+"""
+# Define range
+for i in range(1, 101):
+    # optimal variables for fizzbuzz 
+    fizz = i % 3 == 0  # these variables prevent ugly iff statements
+    buzz = i % 5 == 0   
+    if fizz and buzz:
+        print("Fizzbuzz") # if i is divisible by 3 and 5 print "FizzBuzz"
+    elif fizz:
+        print("Fizz") # if i is divisible by 3 prints "Fizz"
+    elif buzz:
+        print("Buzz") # if i is divisible by 5 prints "Buzz"
+    else:
+        print(i) # when i is not divisible by either prints integer in range
