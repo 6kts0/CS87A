1452a313a7f5b656fb59a710b79d1f1dba746bf0
6kts0 <fabakouyate06@gmail.com>
2026-01-05 16:01:15 -0800

    OpenAI Chatgpt sdk in Python

---
+++ LLMs/gpt.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+API_KEY = 'your-key'
+
+client = OpenAI()
+
+ 
+usr_prompt = input("Prompt: ")
+system_struct = "Do not exceed a 500 word cap in our responses"
+
+response = client.responses.create(
+    input = usr_prompt,
+    instructions = system_struct,
+    model = 'gpt-4.5'
+)
+
+print(response.output_text)
